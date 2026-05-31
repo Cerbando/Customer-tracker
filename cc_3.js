b7b51002d9f11e4924eebc98b2c6787d3044afe7
Cerbando <cmondragonperez@usf.edu>
2026-05-31 12:27:31 -0400

    first commit

---
+++ cc_3.js
@@ -0,0 +1,6 @@
+// Step 2: Create Customer Records
+let customers = [
+    { name: "John Doe", email: "john.doeee@icloud.com", purchases: ["Shoes", "Laptop"] },
+    { name: "Jane Smith", email: "jane.smith4@hotmail.com", purchases: ["Book", "Smart Watch"] },
+    { name: "Rebecca Johnson", email: "rebecca.johnson100@gmail.com", purchases: ["Headphones", "Camera"] }
+];
