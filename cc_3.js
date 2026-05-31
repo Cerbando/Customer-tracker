72b61221ca4c87097dceb3fb58273d7322805855
Cerbando <cmondragonperez@usf.edu>
2026-05-31 12:31:31 -0400

    Added a new customer and removed the first customer

--- cc_3.js
+++ cc_3.js
@@ -4,3 +4,7 @@ let customers = [
     { name: "Jane Smith", email: "jane.smith4@hotmail.com", purchases: ["Book", "Smart Watch"] },
     { name: "Rebecca Johnson", email: "rebecca.johnson100@gmail.com", purchases: ["Headphones", "Camera"] }
 ];
+
+// Step 3: Add and remove Data
+customers.push({ name: "Frangie Arocho", email: "frangie.arocho@gmail.com", purchases: ["Tablet"] });
+customers.shift(); // Remove the first customer (John Doe)
